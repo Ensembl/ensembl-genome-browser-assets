79cbcc18e3206fda0655d36803ff8c05ac8d7912
Dan Sheppard <dan@ebi.ac.uk>
2019-11-12 16:55:51 +0000

    feature/use-npm at 2019-11-12 16:55
    feature/use-npm at 2019-11-12 17:04
    
    feature/use-npm at 2019-11-12 17:08
    
    feature/use-npm at 2019-11-12 18:12
    
    feature/use-npm at 2019-11-12 18:15
    
    feature/use-npm at 2019-11-12 18:17
    
    feature/use-npm at 2019-11-12 18:20
    
    feature/use-npm at 2019-11-12 18:30
    
    feature/use-npm at 2019-11-12 18:37

--- browser.js
+++ browser.js
@@ -19,13 +19,13 @@ if( typeof Rust === "undefined" ) {
         if( typeof process === "object" && typeof process.versions === "object" && typeof process.versions.node === "string" ) {
             var fs = require( "fs" );
             var path = require( "path" );
-            var wasm_path = path.join( __dirname, "/static/browser/browser-7def28c42e83db0db5eacc70ab014add.wasm" );
+            var wasm_path = path.join( __dirname, "/static/browser/browser-4dd67542471141f5d82bde5885b9aaaf.wasm" );
             var buffer = fs.readFileSync( wasm_path );
             var mod = new WebAssembly.Module( buffer );
             var wasm_instance = new WebAssembly.Instance( mod, instance.imports );
             return instance.initialize( wasm_instance );
         } else {
-            var file = fetch( "/static/browser/browser-7def28c42e83db0db5eacc70ab014add.wasm", {credentials: "same-origin"} );
+            var file = fetch( "/static/browser/browser-4dd67542471141f5d82bde5885b9aaaf.wasm", {credentials: "same-origin"} );
 
             var wasm_instance = ( typeof WebAssembly.instantiateStreaming === "function"
                 ? WebAssembly.instantiateStreaming( file, instance.imports )
